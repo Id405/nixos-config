c7e0dd3254b3673d8322605c1c8052c5c467c2a6
Lily <larsens@gmail.com>
2025-12-25 19:14:44 -0800

    Thu Dec 25 07:14:44 PM PST 2025

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -97,15 +97,6 @@ in
     "/".options = [ "compress=zstd" ];
   };
 
-  services.beesd.filesystems = {
-    root = {
-      spec = "/";
-      hashTableSizeMB = 2048;
-      verbosity = "crit";
-      extraOptions = [ "--loadavg-target" "5.0" ];
-    };
-  };
-
   # We need git for nix-rebuild to work
   programs.git.enable = true;
 
